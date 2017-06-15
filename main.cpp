2bab0b546be9e8997267d86825aafdb3f21b55ac
Tom Breza <tom@pcservicegroup.co.uk>
2017-06-15 21:04:26 +0100

    end of today class not very good class :(

--- main.cpp
+++ main.cpp
@@ -1,39 +1,116 @@
+#include <string>
 #include <iostream>
-using namespace std;
+using namespace std	;
 
-template <class T> // T is type so here we are passing INT
 class Part
 {
 private:
-	T a;
-
+	int arr[2];
 public:
-	Part():a(0)
-	{}
-	Part(T b):a(b)
-	{}
-	void display(void)
+
+	class TooBig
 	{
-		cout << a << endl;
+	public:
+		string  sr;
+		TooBig()
+		{}
+
+		TooBig(string kl)
+		{
+			sr = kl;
+		}
+	};
+	void init(int i)
+	{
+		if (i >= 2) throw TooBig("i is bigger then 2");
+		arr[i] = i;
 	}
 };
 
-int main()
+int main(void)
 {
-	int x = 2;
-	Part  <int> part(x);
-	part.display();
+	try
+	{
+		Part p1;
+		p1.init(2);
+	}
 
-	float f = 2.3;
-	Part  <float> part1(f);
-	part1.display();
-
-	string test{"strings works like a dream"};
-	Part  <string> part2(test);
-	part2.display();
-	cin.get();
+	catch(Part::TooBig bg)
+	{cout << bg.sr  << endl	;}
+	//cin.get();
 	return 0;
 }
+
+
+
+/*#include <iostream>
+using namespace std;
+
+double division(int a, int b)
+{
+	if( b == 0 )
+	{
+		throw "Division by zero condition!";
+	}
+	return (a/b);
+}
+
+int main ()
+{
+	int x = 50;
+	int y = 0;
+
+	try {
+		double z = division(x, y);
+		cout << z << endl;
+		cin.get();
+	}
+
+	catch (const char* msg) {
+		cout<< msg << endl;
+		cin.get();
+	}
+	return 0;
+}*/
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//template <class T> // T is type so here we are passing INT
+//class Part
+//{
+//private:
+//	T a;
+//
+//public:
+//	Part():a(0)
+//	{}
+//	Part(T b):a(b)
+//	{}
+//	void display(void)
+//	{
+//		cout << a << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	int x = 2;
+//	Part  <int> part(x);
+//	part.display();
+//
+//	float f = 2.3;
+//	Part  <float> part1(f);
+//	part1.display();
+//
+//	string test{"strings works like a dream"};
+//	Part  <string> part2(test);
+//	part2.display();
+//	cin.get();
+//	return 0;
+//}
 
 
 
